9aae55469b581611748ee0a14cfb374369262cb7
Oohira Suguru <suguru.13579515@gmail.com>
2020-08-09 19:40:00 +0900

    Start Study Python

---
+++ Python.py
@@ -0,0 +1,42 @@
+# coding: UTF-8
+# 「Hello World」と出力してください
+print('Hello World')
+
+# 数値の7を出力してください
+print(7)
+
+# 9に3足した値を出力してください
+print(9+3)
+
+# 「9 + 3」を文字列として出力してください
+print('9+3')
+
+# 9を2で割った値を出力してください
+print(9/2)
+
+# 7に5を掛けた値を出力してください
+print(7*5)
+
+# 5を2で割った時の余りを出力してください
+print(5%2)
+
+# 変数nameに文字列「にんじゃわんこ」を代入してください
+name = 'にんじゃわんこ'
+
+# 変数nameの値を出力してください
+print(name)
+
+# 変数numberに数値の7を代入してください
+number = 7
+
+# 変数numberの値を出力してください
+print(number)
+
+apple_price = 200
+apple_count = 8
+
+# apple_priceとapple_countを掛けた結果を、変数total_priceに代入してください
+total_price = apple_price * apple_count
+
+# total_priceの値を出力してください
+print(total_price)
